e538206439049cb6679ed004b41d0f286764d381
jncinlee <jncinlee@hotmail.com>
2017-05-06 17:10:38 +0200

    Update Keras.py

--- Keras.py
+++ Keras.py
@@ -2,7 +2,7 @@
 #TensorFlow, Theano combine
 #pip install keras
 
-##3 backend; kernel from either tensorflow or theano
+##3 backend
 #theano or tensorflow basis NN
 
 #m1 find the folder 
@@ -13,14 +13,14 @@
 }
 '''
 
-#m2 temp change
+#m2 temp changea
 # import os
 # os.environ['KERAS_BACKEND']='theano'
 # import keras
 
 
 
-##4 regression
+4 regression
 import numpy as np
 np.random.seed(1337)  # for reproducibility
 from keras.models import Sequential #sequ layer
@@ -203,4 +203,235 @@ print('\ntest accuracy: ', accuracy)
 
 
 
+##8 RNN
+import numpy as np
+np.random.seed(1337)  # for reproducibility
+
+from keras.datasets import mnist
+from keras.utils import np_utils
+from keras.models import Sequential
+from keras.layers import SimpleRNN, Activation, Dense
+from keras.optimizers import Adam
+
+TIME_STEPS = 28     # same as the height of the image
+INPUT_SIZE = 28     # same as the width of the image
+BATCH_SIZE = 50
+BATCH_INDEX = 0
+OUTPUT_SIZE = 10
+CELL_SIZE = 50
+LR = 0.001
+
+
+# download the mnist to the path '~/.keras/datasets/' if it is the first time to be called
+# X shape (60,000 28x28), y shape (10,000, )
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+
+# data pre-processing
+X_train = X_train.reshape(-1, 28, 28) / 255.      # normalize
+X_test = X_test.reshape(-1, 28, 28) / 255.        # normalize
+y_train = np_utils.to_categorical(y_train, num_classes=10)
+y_test = np_utils.to_categorical(y_test, num_classes=10)
+
+# build RNN model
+model = Sequential()
+
+# RNN cell
+model.add(SimpleRNN(
+    # for batch_input_shape, if using tensorflow as the backend, we have to put None for the batch_size.
+    # Otherwise, model.evaluate() will get error.
+    batch_input_shape=(None, TIME_STEPS, INPUT_SIZE),
+    # Or: input_dim=INPUT_SIZE, input_length=TIME_STEPS,
+    output_dim=CELL_SIZE,
+    unroll=True,
+))
+
+# output layer
+model.add(Dense(OUTPUT_SIZE))
+model.add(Activation('softmax'))
+
+# optimizer
+adam = Adam(LR)
+model.compile(optimizer=adam,
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
+
+# training
+for step in range(4001):
+    # data shape = (batch_num, steps, inputs/outputs)
+    X_batch = X_train[BATCH_INDEX: BATCH_INDEX+BATCH_SIZE, :, :]
+    Y_batch = y_train[BATCH_INDEX: BATCH_INDEX+BATCH_SIZE, :]
+    cost = model.train_on_batch(X_batch, Y_batch)
+    BATCH_INDEX += BATCH_SIZE
+    BATCH_INDEX = 0 if BATCH_INDEX >= X_train.shape[0] else BATCH_INDEX
+
+    if step % 500 == 0:
+        cost, accuracy = model.evaluate(X_test, y_test, batch_size=y_test.shape[0], verbose=False)
+        print('test cost: ', cost, 'test accuracy: ', accuracy)
+
+
+
+##9 LSTM RNN
+#avoid gd explode or disappear
+#use more gate for learning
+import numpy as np
+np.random.seed(1337)  # for reproducibility
+import matplotlib.pyplot as plt
+from keras.models import Sequential
+from keras.layers import LSTM, TimeDistributed, Dense
+from keras.optimizers import Adam
+
+BATCH_START = 0
+TIME_STEPS = 20
+BATCH_SIZE = 50
+INPUT_SIZE = 1
+OUTPUT_SIZE = 1
+CELL_SIZE = 20
+LR = 0.006
+
+
+def get_batch():
+    global BATCH_START, TIME_STEPS
+    # xs shape (50batch, 20steps)
+    xs = np.arange(BATCH_START, BATCH_START+TIME_STEPS*BATCH_SIZE).reshape((BATCH_SIZE, TIME_STEPS)) / (10*np.pi)
+    seq = np.sin(xs)
+    res = np.cos(xs)
+    BATCH_START += TIME_STEPS
+    # plt.plot(xs[0, :], res[0, :], 'r', xs[0, :], seq[0, :], 'b--')
+    # plt.show()
+    return [seq[:, :, np.newaxis], res[:, :, np.newaxis], xs]
+
+model = Sequential()
+
+# build a LSTM RNN
+model.add(LSTM(
+    batch_input_shape=(BATCH_SIZE, TIME_STEPS, INPUT_SIZE),
+    # Or: input_dim=INPUT_SIZE, input_length=TIME_STEPS,
+    output_dim=CELL_SIZE, #20 knots
+    return_sequences=True,      # True: output at all steps. False: output as last step.
+    stateful=True,              # True: the final state of batch1 is feed into the initial state of batch2
+    #batch connect to each other? curve is conti Ture
+))
+# add output layer
+model.add(TimeDistributed(Dense(OUTPUT_SIZE))) #full connect, everytime pt
+adam = Adam(LR)
+model.compile(optimizer=adam,
+              loss='mse',)
+
+print('Training ------------')
+for step in range(501):
+    # data shape = (batch_num, steps, inputs/outputs)
+    X_batch, Y_batch, xs = get_batch()
+    cost = model.train_on_batch(X_batch, Y_batch)
+    pred = model.predict(X_batch, BATCH_SIZE)
+    plt.plot(xs[0, :], Y_batch[0].flatten(), 'r', xs[0, :], pred.flatten()[:TIME_STEPS], 'b--')
+    plt.ylim((-1.2, 1.2))
+    plt.draw()
+    plt.pause(0.1)
+    if step % 10 == 0:
+        print('train cost: ', cost)
+
+
+
+##10 Autoencoder
+#compress>>core>>decompress
+#unsuperise method only need x
+
+import numpy as np
+np.random.seed(1337)  # for reproducibility
+
+from keras.datasets import mnist
+from keras.models import Model
+from keras.layers import Dense, Input
+import matplotlib.pyplot as plt
+
+# download the mnist to the path '~/.keras/datasets/' if it is the first time to be called
+# X shape (60,000 28x28), y shape (10,000, )
+(x_train, _), (x_test, y_test) = mnist.load_data()
+
+# data pre-processing
+x_train = x_train.astype('float32') / 255. - 0.5       # minmax_normalized
+x_test = x_test.astype('float32') / 255. - 0.5         # minmax_normalized
+x_train = x_train.reshape((x_train.shape[0], -1))
+x_test = x_test.reshape((x_test.shape[0], -1))
+print(x_train.shape)
+print(x_test.shape)
+
+# in order to plot in a 2D figure
+encoding_dim = 2
+
+# this is our input placeholder
+input_img = Input(shape=(784,))
+
+# encoder layers
+encoded = Dense(128, activation='relu')(input_img)
+encoded = Dense(64, activation='relu')(encoded)
+encoded = Dense(10, activation='relu')(encoded)
+encoder_output = Dense(encoding_dim)(encoded)
+
+# decoder layers
+decoded = Dense(10, activation='relu')(encoder_output)
+decoded = Dense(64, activation='relu')(decoded)
+decoded = Dense(128, activation='relu')(decoded)
+decoded = Dense(784, activation='tanh')(decoded) #tanh to (-1,1) range
+
+# construct the autoencoder model
+autoencoder = Model(input=input_img, output=decoded)
+
+# construct the encoder model for plotting
+encoder = Model(input=input_img, output=encoder_output)
+
+# compile autoencoder
+autoencoder.compile(optimizer='adam', loss='mse')
+
+# training
+autoencoder.fit(x_train, x_train,
+                nb_epoch=20,
+                batch_size=256,
+                shuffle=True)
+
+# plotting
+encoded_imgs = encoder.predict(x_test)
+plt.scatter(encoded_imgs[:, 0], encoded_imgs[:, 1], c=y_test)
+plt.colorbar()
+plt.show()
+
+
+
+##11 Save load
+import numpy as np
+np.random.seed(1337)  # for reproducibility
+
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.models import load_model
+
+# create some data
+X = np.linspace(-1, 1, 200)
+np.random.shuffle(X)    # randomize the data
+Y = 0.5 * X + 2 + np.random.normal(0, 0.05, (200, ))
+X_train, Y_train = X[:160], Y[:160]     # first 160 data points
+X_test, Y_test = X[160:], Y[160:]       # last 40 data points
+model = Sequential()
+model.add(Dense(output_dim=1, input_dim=1))
+model.compile(loss='mse', optimizer='sgd')
+for step in range(301):
+    cost = model.train_on_batch(X_train, Y_train)
+
+# save
+print('test before save: ', model.predict(X_test[0:2]))
+model.save('my_model.h5')   # HDF5 file, you have to pip3 install h5py if don't have it
+del model  # deletes the existing model
+
+# load
+model = load_model('my_model.h5')
+print('test after load: ', model.predict(X_test[0:2]))
+"""
+# save and load weights
+model.save_weights('my_model_weights.h5')
+model.load_weights('my_model_weights.h5')
+# save and load fresh network without trained weights
+from keras.models import model_from_json
+json_string = model.to_json()
+model = model_from_json(json_string)
+"""
 
